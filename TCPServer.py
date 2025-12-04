fad3feb5f85203d816397b75ac39a5d43a9760c5
tyxcd <147106396+tyxcd@users.noreply.github.com>
2025-12-04 21:45:56 +0000

    git push origin main

--- TCPServer.py
+++ TCPServer.py
@@ -1,25 +1,46 @@
 import socket
 import ssl
 from socket import *
-serverName = '10.197.133.25'
-serverPort = 12000
-####NEW tls portion
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT) #ssl context for client
-context.load_verify_locations('server.crt') #load server certificate
-##end new tls portion
-raw_socket = socket(AF_INET, SOCK_STREAM)#create raw socjket
+serverPort = 12000 
+serverIP = '' #will listen on all if including current
 
-clientSocket = context.wrap_socket(raw_socket, server_hostname=serverName)#make sure server hostname matches ip in certificate
-print("connecting...")
-clientSocket.connect((serverName, serverPort)) #TCP connection to server
-print("connected!")
-#clientSocket = socket(AF_INET, SOCK_STREAM)
-#clientSocket.connect((serverName, serverPort)) #TCP connection to server
+# --- new tls ---
+#create ssl context
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+#load generated cert and key in same folder
+context.load_cert_chain(certfile='server.crt', keyfile='server.key')
+# --- NEW TLS SETUP END ---
 
-sentence = input('Input sentence:') 
-clientSocket.send(sentence.encode())
-#wait for response
-sentence = clientSocket.recv(1024)
-print('From Server:', sentence.decode())
-clientSocket.close()
-# TCP client program
+serverSocket = socket(AF_INET, SOCK_STREAM) #tcp welcome socket creation
+serverSocket.bind((serverIP, serverPort)) #bind to address and port
+serverSocket.listen(1) #server listens for tcp requests
+print('The server is ready to receive')
+
+while True: #infinite loop for server
+    #accept tcp connection
+    raw_socket, addr = serverSocket.accept() #changed variable name to raw socket from previous
+    
+    print('Connection from:', addr) #display client address when request is received
+    #wrap with tls (handshake)
+    try: #try block to handle client communication
+        connectionSocket = context.wrap_socket(raw_socket, server_side=True)
+    except ssl.SSLError as e:
+        print(f"SSL Handshake failed: {e}")
+        raw_socket.close()
+        continue
+
+    #end updated portion now original from part 1
+
+    try:
+        sentence = connectionSocket.recv(1024).decode() #read bytes from socket
+        if not sentence: #check for empty message
+            print('Client disconnected.') 
+        else:#
+            connectionSocket.send(sentence.encode())
+    except (ConnectionResetError, BrokenPipeError) as e: #handle connection errors, broken pipe is for sending on closed connection
+        print(f"An error occurred with {addr}: {e}") #detailed error message
+    finally:
+        connectionSocket.close() #close connection socket
+        print(f"Connection with {addr} closed.")
+
+
